6c99684ba6d0fa0548318f61dcd2f70da517f449
carlon702 <carlosj.marchal@gmail.com>
2024-04-09 17:10:54 -0300

    feat:register, actions and reducers

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,35 @@
+import {Component} from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { Store } from "@ngrx/store";
+import { register } from "../../store/actions";
+import { RegisterRequestInterface } from "../../types/registerRequest.interface";
+import { RouterLink } from "@angular/router";
+import { AuthStateInterface } from "../../types/authState.interface";
+import { CommonModule } from "@angular/common";
+import { selectIsSubmitting } from "../../store/reducers";
+
+@Component({
+    selector: "mc-register",
+    standalone: true,
+    templateUrl: "./register.component.html",
+    imports:[ReactiveFormsModule, RouterLink, CommonModule]
+})
+
+export class RegisterComponent {
+    form = this.fb.nonNullable.group({
+        username: ["", Validators.required],
+        email: ["", Validators.required],
+        password: ["", Validators.required],
+    })
+    isSubmitting$ = this.store.select(selectIsSubmitting)
+
+    constructor(private fb: FormBuilder, private store: Store<{auth: AuthStateInterface}>) {}
+
+    onSubmit(){
+        console.log("form", this.form.getRawValue())
+        const request: RegisterRequestInterface = {
+            user: this.form.getRawValue()
+        }
+        this.store.dispatch(register({request}))
+    }
+}
