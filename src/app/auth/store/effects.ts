6da49b2900d06432d2dac528c6d9dda6995252ab
carlos marchal <carlosj.marchal@gmail.com>
2024-04-16 10:46:50 -0300

    finished register effects

--- src/app/auth/store/effects.ts
+++ src/app/auth/store/effects.ts
@@ -1,28 +1,48 @@
-import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { AuthService } from "../services/auth.service";
-import { authActions } from "./actions";
-import { catchError, map, of, switchMap } from "rxjs";
-import { inject } from "@angular/core";
-import { CurrentUserInterface } from "src/app/shared/types/currentUser.interface";
-import { HttpErrorResponse } from "@angular/common/http";
+import {Actions, createEffect, ofType} from '@ngrx/effects';
+import {AuthService} from '../services/auth.service';
+import {authActions} from './actions';
+import {catchError, map, of, switchMap, tap} from 'rxjs';
+import {inject} from '@angular/core';
+import {CurrentUserInterface} from 'src/app/shared/types/currentUser.interface';
+import {HttpErrorResponse} from '@angular/common/http';
+import {PersistanceService} from 'src/app/shared/services/persistance.service';
+import {Router} from '@angular/router';
 
 export const registerEffect = createEffect(
-    (actions$ = inject(Actions), authService = inject(AuthService)) => {
-        return actions$.pipe(
-            ofType(authActions.register),
-            switchMap(({request}) => {
-                return authService.register(request).pipe(
-                    map((currentUser: CurrentUserInterface)=>{
-                        return authActions.registerSuccess({currentUser})
-                    }),
-                    catchError((errorResponse:HttpErrorResponse)=>{
-                        return of(authActions.registerFailure({
-                            errors:errorResponse.error.errors
-                        }))
-                    })
-                )
-            })
-        )
-    },
-    {functional: true}
-)
+  (
+    actions$ = inject(Actions),
+    authService = inject(AuthService),
+    persistanceService = inject(PersistanceService),
+  ) => {
+    return actions$.pipe(
+      ofType(authActions.register),
+      switchMap(({request}) => {
+        return authService.register(request).pipe(
+          map((currentUser: CurrentUserInterface) => {
+            persistanceService.set('accesToken', currentUser.token);
+            return authActions.registerSuccess({currentUser});
+          }),
+          catchError((errorResponse: HttpErrorResponse) => {
+            return of(
+              authActions.registerFailure({
+                errors: errorResponse.error.errors,
+              }),
+            );
+          }),
+        );
+      }),
+    );
+  },
+  {functional: true},
+);
+
+export const redirectAfterRegisterEffect = createEffect(
+  (actions$ = inject(Actions), router = inject(Router)) => {
+    return actions$.pipe(
+      ofType(authActions.registerSuccess),
+      tap(() => {
+        router.navigateByUrl('/');
+      }),
+    );
+  },
+);
