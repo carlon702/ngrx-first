ab31bfaf8a89e329d35e594ce2faf340586ec56e
carlon702 <carlosj.marchal@gmail.com>
2024-04-16 09:28:59 -0300

    finished register component, implemented errors

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,19 +1,20 @@
 import {Component} from "@angular/core";
 import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { Store } from "@ngrx/store";
-import { register } from "../../store/actions";
 import { RegisterRequestInterface } from "../../types/registerRequest.interface";
 import { RouterLink } from "@angular/router";
-import { AuthStateInterface } from "../../types/authState.interface";
 import { CommonModule } from "@angular/common";
-import { selectIsSubmitting } from "../../store/reducers";
-import { AuthService } from "../../services/auth.service";
+import { selectIsSubmitting, selectValidationErrors } from "../../store/reducers";
+import { authActions } from "../../store/actions";
+import { BackendErrorMessages } from "src/app/shared/components/backendErrorMessages/backendErrorMessages.component";
+import { combineLatest } from "rxjs";
+
 
 @Component({
     selector: "mc-register",
     standalone: true,
     templateUrl: "./register.component.html",
-    imports:[ReactiveFormsModule, RouterLink, CommonModule]
+    imports:[ReactiveFormsModule, RouterLink, CommonModule, BackendErrorMessages]
 })
 
 export class RegisterComponent {
@@ -22,7 +23,10 @@ export class RegisterComponent {
         email: ["", Validators.required],
         password: ["", Validators.required],
     })
-    isSubmitting$ = this.store.select(selectIsSubmitting)
+    data$ = combineLatest({
+       isSubmitting: this.store.select(selectIsSubmitting),
+       backendErrors: this.store.select(selectValidationErrors),
+    })
 
     constructor(
         private fb: FormBuilder,
@@ -34,6 +38,6 @@ export class RegisterComponent {
         const request: RegisterRequestInterface = {
             user: this.form.getRawValue()
         }
-        this.store.dispatch(register({request}))
+        this.store.dispatch(authActions.register({request}))
     }
 }
--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,4 +1,21 @@
-import { createAction, props } from "@ngrx/store";
+import { createActionGroup, props } from "@ngrx/store";
 import { RegisterRequestInterface } from "../types/registerRequest.interface";
+import { CurrentUserInterface } from "src/app/shared/types/currentUser.interface";
+import { BackendErrorsInterface } from "src/app/shared/types/backendErrors.interface";
 
-export const register = createAction("[Auth] Register", props<{request: RegisterRequestInterface}>())
+// export const register = createAction("[Auth] Register", props<{request: RegisterRequestInterface}>())
+
+// export const registerSuccess = createAction("[Auth] Register Success", props<{request: RegisterRequestInterface}>())
+
+// export const registerFailure = createAction("[Auth] Register Failure", props<{request: RegisterRequestInterface}>())
+
+
+//Syntax for grouping actions = less code
+export const authActions = createActionGroup({
+    source:"auth",
+    events:{
+        Register:props<{request: RegisterRequestInterface}>(),
+        'Register Success': props<{currentUser: CurrentUserInterface}>(),
+        'Register Failure': props<{errors: BackendErrorsInterface}>(),
+    }
+})
